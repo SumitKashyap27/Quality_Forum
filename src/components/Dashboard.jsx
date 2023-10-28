e73a477971683b401ba6b78dfa87aa8e033a4a77
SumitKashyap27 <sumit20020927@gmail.com>
2023-10-28 23:15:42 +0530

    commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -24,27 +24,30 @@ const drawerWidth = 240;
 function ResponsiveDrawer(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [currentTab,setCurrentTab] = React.useState("Dashboard")
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
+
+
   const drawer = (
     <div>
       <Toolbar />
       <img
-        className='pt-0 py-2 px-4'
+        className='pt-0 mt-0 py-2 px-4'
         loading="lazy"
         src={Studentimg}
         alt="Student"
         />
-        <h3 className='pt-0 pb-3 justify-center items-center'>Name</h3>
-      <Divider />
+    <h3 className='pt-2 pb-3  text-center'>Name</h3>
 
+    <Divider />
       <List>
-        {['Dashboard', 'problems arised', 'previous'].map((text, index) => (
+        {['Dashboard', 'Feedback'].map((text, index) => (
           <ListItem key={text} disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={()=>setCurrentTab(text)}>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
@@ -54,9 +57,13 @@ function ResponsiveDrawer(props) {
         ))}
       </List>
       <Divider />
+      <Divider />
+      <div className='pb-0'>
+        <h2>Quality Forum</h2>
+      </div>
     </div>
   );
-
+  console.log(currentTab)
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
@@ -80,14 +87,13 @@ function ResponsiveDrawer(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+            Responsive drawer 
           </Typography>
         </Toolbar>
       </AppBar>
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
       >
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
@@ -121,43 +127,15 @@ function ResponsiveDrawer(props) {
         sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
       >
         <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+          {currentTab === "Dashboard" && <>Dashboard</>}
+          {currentTab === "Feedback" && <>Feedback</>}
+          {/* {currentTab === "previous" && <>Previous</>} */}
       </Box>
     </Box>
   );
 }
 
 ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window: PropTypes.func,
 };
 
